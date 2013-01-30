1fcda6404aec48d84cdbf9123c2692d00aa0cc93
Kaitlyn Parkhurst <symkat@symkat.com>
2013-01-29 16:10:05 -0800

    Updated for the sake of my blog not falling apart.  Don't use this codebase.

--- lib/Net/GitHub/Markdown.pm
+++ lib/Net/GitHub/Markdown.pm
@@ -4,6 +4,7 @@ use strict;
 use WWW::Mechanize;
 use HTML::TreeBuilder;
 my $mech;
+use Data::Dumper;
 
 our $VERSION = '0.001000'; # 0.1.0
 $VERSION = eval $VERSION;
@@ -17,9 +18,9 @@ sub markdown {
     my $html = $self->html_from_gist(
         $self->create_gist($content)
     );
-    $html =~ s/^<div class="blob instapaper_body" id="readme">//;
-    $html =~ s/<\/div>$//;
-    $html =~ s/^ <div class="wikistyle">/<div id="markdown">/;
+    $html =~ s/^<div id="readme">/\n <div class="wikistyle">/;
+    $html .= "\n";
+
     return $html;
 }
 
@@ -38,32 +39,17 @@ sub mech {
 sub create_gist {
     my ( $self, $content ) = @_;
     $self->mech->get( "http://gist.github.com/" );
+
     $self->mech->submit_form(
-        form_number => 2,
+        form_number => 1,
         fields      => {
-            'file_contents[gistfile1]' => $content,
-            'file_ext[gistfile1]'      => '.md',
+            'gist[files][][content]'  => $content,
+            'gist[files][][language]' => 'Markdown',
+            'gist[public]'            => 0,
         },
     );
 
-    $content = $self->mech->content;
-    
-    if ( $self->mech->title =~ /gist:\s+([a-f0-9]+)\s+/ ) {
-        my $id = $1;
-        $self->mech->post( "https://gist.github.com/delete/$id", 
-            {
-                '_method' => 'delete',
-                'authenticity_token' => $self->_get_auth_token($content),
-            }
-        );
-        my $status = $self->mech->status;
-        warn "Expected 200, but got $status while deleting the gist." 
-            unless $status == 200;
-    } else {
-        warn "Expected an ID, but couldn't find one. Please report this.";
-    }
-
-    return $content;
+    return $self->mech->content;
 }
 
 sub html_from_gist {
@@ -72,22 +58,6 @@ sub html_from_gist {
     my $content = $root->look_down( _tag => 'div', "id" => "readme" )->as_HTML(undef, ' ', {});
     $root->delete;
     return $content;
-}
-
-# Deleting gists is ugly and NOT cheap.  This is
-# due to GitHub using JS to automagically create
-# the HTML form and force me to look through HTML
-# to find it.
-
-sub _get_auth_token {
-    my ( $self, $html ) = @_;
-    my $root = HTML::TreeBuilder->new_from_content($html);
-    my $content = $root->look_down( _tag => 'div', 'id' => 'delete_link' )->as_HTML;
-
-    if ( $content =~ /([a-f0-9]{40})/ ) {
-        return $1;
-    }
-    warn "Deleting Gists is not functioning correctly. Please report this.";
 }
 
 1;
